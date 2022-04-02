cb1e21a0d5439cce986dc29449c8d615c2cb8b24
rishyak <hello@rishyak.com>
2022-04-02 16:56:19 -0400

    Doxygen and main structure

---
+++ 8puzzle.py
@@ -0,0 +1,24 @@
+"""
+Authors:
+Juan Jose Castano Moreno
+jc10536@nyu.edu
+
+Rishyak Panchal
+rishyak@nyu.edu
+"""
+
+
+"""
+* @brief Lorem ipsum dolor sit amet
+*
+* @param p1: Lorem
+* @param p2: Lorem
+*
+* @returns Return val
+"""
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # 8puzzle
 NYU Tandon's CS-UY 4613 Intro to Artificial Intelligence Project 1
+
+## Authors
+Juan Jose Castano Moreno   
+jc10536@nyu.edu   
+
+Rishyak Panchal   
+rishyak@nyu.edu   
