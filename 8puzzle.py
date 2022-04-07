a83b527189a8d97865ac19bace14c9c8ea95d7d7
juanjomoreno11 <castanomorenoj@gmail.com>
2022-04-07 19:04:49 -0400

    algorithm class created
    Algorithm class created and expand deleted from 8puzzle file

--- 8puzzle.py
+++ 8puzzle.py
@@ -111,50 +111,6 @@ def nilssonScore(currentState: list[int], goalState: list[int]) -> int:
     return pn + 3 * sn
 
 
-def expand(expandNode : Node, activeNodes : list[Node], visitedStates : list[list[int]]) -> None:
-    """ Creates the current node's children.
-
-    Args:
-        expandNode (Node): Node that we want to expand.
-        activeNodes (list[int]): List of nodes that are yet to be expanded.
-        visitedNodes (list[list[int]]): List of already visited nodes.
-
-    Returns:
-        None. Function modifies the activeNodes and the visitedStates lists.
-    """
-    emptyIndex = expandNode.state.index(0)
-
-    if(emptyIndex == 4):
-        newState1 = expandNode.state
-        newState1[1], newState1[4] = newState1[4], newState1[1]
-        move1 = [1, 4, newState1[4]] # Moves are represented with the old index, the new index, and the item that was moved
-
-        newState2 = expandNode.state
-        newState2[3], newState2[4] = newState2[4], newState2[3]
-        move2 = [3,4,newState2[4]]
-
-        newState3 = expandNode.state
-        newState3[5], newState3[4] = newState3[4], newState3[5]
-        move3 = [5, 4, newState3[4]]
-
-        newState4 = expandNode.state
-        newState4[7], newState4[4] = newState4[4], newState4[7]
-        move4 = [7,4, newState4[4]]
-
-        if (newState1 not in visitedStates):
-            newNode1 = Node(newState1, expandNode, expandNode.depth+1, move1)
-            activeNodes.append(newNode1)
-        if (newState2 not in visitedStates):
-            newNode2 = Node(newState2, expandNode, expandNode.depth+1, move2)
-            activeNodes.append(newNode2)
-        if (newState3 not in visitedStates):
-            newNode3 = Node(newState3, expandNode, expandNode.depth+1, move3)
-            activeNodes.append(newNode3)
-        if (newState4 not in visitedStates):
-            newNode4 = Node(newState4, expandNode, expandNode.depth+1, move4)
-            activeNodes.append(newNode4)
-
-
 def aStarSearch(currentState : Node, goalState : list[int]) -> list:
     """ Takes initial and goal states calculates the 
         shortest path to the goal state
@@ -174,7 +130,7 @@ def aStarSearch(currentState : Node, goalState : list[int]) -> list:
 
     while(len(activeNodes) != 0):
         currentState = activeNodes.pop
-        expand(currentState, activeNodes, visitedStates)
+        #expand(currentState, activeNodes, visitedStates)
 
 
 def main() -> None:
---
+++ Algorithm.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+from 8puzzle import *
+
+class Algorithm:
+    def __init__(self):
+        self.active = []
+        self.visited = []
+
+    def insertActive(self, stateNode):
+        self.active.append(stateNode) #Figure out the rest later
+
+        for i in range(len(active)-1, 1, -1):
+            if(active[i].pathcost > active[i-1].pathcost):
+                active[i], active[i-1] = active[i-1], active[i]
+
+
+
+    def expand(self, expandNode : Node) -> None:
+        """ Creates the current node's children.
+
+        Args:
+            expandNode (Node): Node that we want to expand.
+            activeNodes (list[int]): List of nodes that are yet to be expanded.
+            visitedNodes (list[list[int]]): List of already visited nodes.
+
+        Returns:
+            None. Function modifies the activeNodes and the visitedStates lists.
+        """
+        emptyIndex = expandNode.board.state.index(0)
+        emptyRow = emptyIndex // 3
+        emptyCol = emptyIndex % 3
+
+        for index in range(len(expandNode.state)):
+
+            currRow = index // 3
+            currCol = index % 3
+
+            if (((abs(currRow - emptyRow)) == 1) or (abs(currCol - emptyCol) == 1)):
+                newState = expandNode.board.state
+                newState[index], newState[emptyIndex] = newState[emptyIndex], newState[index]
+                verticalMove = currRow - emptyRow
+                horizontalMove = currCol - emptyCol
+
+                if(verticalMove < 0):
+                    move = "down"
+                elif(verticalMove > 0)
+                    move = "up"
+                elif(horizontalMove < 0):
+                    move = "right"
+                elif(horizontalMove > 0):
+                    move = "left"
+
+                newNode = Node(newState, expandNode.goal, 1, expandNode, expandNode.depth + 1, move)
+                if(newNode not in self.visited):
+                    self.insertActive(newNode)
+        self.visited.append(expandNode)
